50043e0a84c340b226482d0f8e354feabae4c34c
Paradoxed <Vikram.sh86@gmail.com>
2017-02-08 20:44:06 +0100

    Added Image Assets + UIColor additions

---
+++ iFast/iFast/UIColor + FastAdditions.swift
@@ -0,0 +1,115 @@
+//
+//  UIColor + FastAdditions.swift
+//  iFast
+//
+//  Created by Suthananth Arulanatham on 08.02.2017.
+//  Copyright © 2017 Suthananth Arulanatham. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  class func RGBColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+    return UIColor(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha)
+  }
+  class var FastWhite: UIColor {
+    return UIColor(white: 255.0 / 255.0, alpha: 1.0)
+  }
+  
+  class var FastHeather: UIColor {
+    return RGBColor(red: 173.0, green: 168.0, blue: 190.0, alpha: 1.0)
+  }
+  
+  class var FastSeafoamBlue: UIColor {
+    return RGBColor(red: 121, green: 190, blue: 209, alpha: 1)
+  }
+  
+  class var FastLightGreyBlue: UIColor {
+    return RGBColor(red: 149.0, green: 184.0, blue: 209.0, alpha: 1.0)
+  }
+  
+  class var FastLightBlueGrey: UIColor {
+    return RGBColor(red: 198.0, green: 202.0, blue: 237.0, alpha: 1.0)
+  }
+  
+  class var FastBeige: UIColor {
+    return RGBColor(red: 239.0, green: 199.0, blue: 194.0, alpha: 1.0)
+  }
+  
+  class var FastPurpleyGrey: UIColor {
+    return RGBColor(red: 150.0, green: 145.0, blue: 149.0, alpha: 1.0)
+  }
+  
+  class var FastFadedBlue: UIColor {
+    return RGBColor(red: 128.0, green: 155.0, blue: 206.0, alpha: 1.0)
+  }
+  
+  class var FastSilver: UIColor {
+    return RGBColor(red: 191.0, green: 211.0, blue: 193.0, alpha: 1.0)
+  }
+  
+  class var FastCloudyBlue: UIColor {
+    return RGBColor(red: 185.0, green: 216.0, blue: 194.0, alpha: 1.0)
+  }
+  
+  class var FastLightGreyBlueTwo: UIColor {
+    return RGBColor(red: 154.0, green: 194.0, blue: 201.0, alpha: 1.0)
+  }
+  
+  class var FastLightTeal: UIColor {
+    return RGBColor(red: 180.0, green: 237.0, blue: 210.0, alpha: 1.0)
+  }
+  
+  class var FastLightGreyBlueThree: UIColor {
+    return RGBColor(red: 160.0, green: 207.0, blue: 211.0, alpha: 1.0)
+  }
+  
+  class var FastGreyishPink: UIColor {
+    return RGBColor(red: 198.0, green: 135.0, blue: 143.0, alpha: 1.0)
+  }
+  
+  class var FastMushroom: UIColor {
+    return RGBColor(red: 183.0, green: 157.0, blue: 148.0, alpha: 1.0)
+  }
+  
+  class var FastGreyBlue: UIColor {
+    return RGBColor(red: 113.0, green: 128.0, blue: 172.0, alpha: 1.0)
+  }
+  
+  class var FastDarkSlateBlue: UIColor {
+    return RGBColor(red: 43.0, green: 69.0, blue: 112.0, alpha: 1.0)
+  }
+  
+  class var FastCharcoalGrey: UIColor {
+    return RGBColor(red: 56.0, green: 63.0, blue: 81.0, alpha: 1.0)
+  }
+  
+  class var FastDusk: UIColor {
+    return RGBColor(red: 60.0, green: 79.0, blue: 118.0, alpha: 1.0)
+  }
+  
+  class var FastPaleTeal: UIColor {
+    return RGBColor(red: 160.0, green: 208.0, blue: 174.0, alpha: 1.0)
+  }
+  
+  class var FastLightTealTwo: UIColor {
+    return RGBColor(red: 129.0, green: 227.0, blue: 215.0, alpha: 1.0)
+  }
+  
+  class var FastTiffanyBlue: UIColor {
+    return RGBColor(red: 132.0, green: 221.0, blue: 236.0, alpha: 1.0)
+  }
+  
+  class var FastPaleGrey: UIColor {
+    return RGBColor(red: 236.0, green: 235.0, blue: 236.0, alpha: 1.0)
+  }
+  
+  class var FastPaleGreyTwo: UIColor {
+    return RGBColor(red: 246.0, green: 245.0, blue: 246.0, alpha: 1.0)
+  }
+  
+  class var FastSquash: UIColor {
+    return RGBColor(red: 246.0, green: 166.0, blue: 35.0, alpha: 1.0)
+  }
+}
