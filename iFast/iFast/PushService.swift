67fd71d6ef4694ae3f3201f14b7491473c15851e
Suthananth Arulanatham <Vikram.sh86@gmail.com>
2017-02-13 20:27:18 +0100

    Updated file structure

---
+++ iFast/iFast/PushService.swift
@@ -0,0 +1,13 @@
+//
+//  PushService.swift
+//  iFast
+//
+//  Created by Suthananth Arulanatham on 13.02.2017.
+//  Copyright © 2017 Suthananth Arulanatham. All rights reserved.
+//
+
+import UIKit
+
+class PushService {
+
+}
