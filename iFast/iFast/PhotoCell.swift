142ea83c85eb5cd80484328c01046dd8c045dd80
Suthananth Arulanatham <Vikram.sh86@gmail.com>
2017-03-05 21:03:04 +0100

    Added Photo collection view

---
+++ iFast/iFast/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  iFast
+//
+//  Created by Suthananth Arulanatham on 05.03.2017.
+//  Copyright © 2017 Suthananth Arulanatham. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ iFast/iFast/PhotoCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoCellViewModel.swift
+//  iFast
+//
+//  Created by Suthananth Arulanatham on 05.03.2017.
+//  Copyright © 2017 Suthananth Arulanatham. All rights reserved.
+//
+
+import Foundation
